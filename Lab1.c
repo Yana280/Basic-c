4059f8dc7039d4edb7ce1f1737f966a9cabcdb6f
Yana280 <125993593+Yana280@users.noreply.github.com>
2023-04-08 15:53:21 +0530

    Add files via upload

---
+++ Lab1.c
@@ -0,0 +1,20 @@
+
+
+
+
+#include <stdio.h> // Need to include this header for printf function
+
+int main()
+{
+    int x = 1;
+    char y = 'c'; // Use single quotes for characters
+    float z = 5.5;
+
+    printf("hello world!\n"); // Added \n to print on a new line
+    printf("%d\n", x); // Added \n to print on a new line
+    printf("%c\n", y); // Added \n to print on a new line
+    printf("%f\n", z); // Added \n to print on a new line
+
+    return 0; // Need to return a value since main is of type explain this code
+}
+
