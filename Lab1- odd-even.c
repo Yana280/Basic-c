9f14021f09d4294a887c04c33ae8e88e51ccaa39
Yana280 <125993593+Yana280@users.noreply.github.com>
2023-04-08 22:55:07 +0530

    Add files via upload

---
+++ Lab1- odd-even.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    }
+    else {
+        printf("%d is odd.\n", num);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
