3615e19f97bc374a7f6a50a19464f2322fdefb88
Yana280 <125993593+Yana280@users.noreply.github.com>
2023-04-08 23:59:31 +0530

    Add files via upload

---
+++ Lab1- input from user.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+   int num;
+
+   // prompt the user to enter an integer
+   printf("Enter an integer: ");
+
+   // read the integer input from the user
+   scanf("%d", &num);
+
+   // print the integer entered by the user
+   printf("You entered: %d\n", num);
+
+   return 0;
+}
+
