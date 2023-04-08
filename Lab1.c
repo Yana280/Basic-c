f3750ec50d4535fb0c330e72f85f0ab4204dd566
Yana280 <125993593+Yana280@users.noreply.github.com>
2023-04-08 10:49:35 +0530

    Add files via upload

---
+++ Lab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+  int x = 1;
+  char y= "c";
+  float z = 1.1;
+  
+  printf("hello world!");
+  printf("%d",x);
+  printf("%c",y);
+  printf("%f",z);
+
+}
